e7b2d448c6acf1eceef75c3bb78c64063f3fb62d
Bruna:3 <pereira.bruna27@escola.pr.gov.br>
2026-06-17 08:53:33 -0300

    S

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função simples para simular a ação de compra
+function comprar(nomeDrone) {
+    alert(`Obrigado pelo interesse no ${nomeDrone}! Um de nossos consultores agrícolas entrará em contato em breve para fechar o orçamento.`);
+}
